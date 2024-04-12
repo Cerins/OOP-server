9ccc39e70e3163c9fbc1e4bcc77b2782d5561e6a
Roberts Cerins <cerins4141@gmail.com>
2024-04-12 09:40:52 +0300

    First commit

---
+++ src/main/java/lu/oop/server/controller/HelloController.java
@@ -0,0 +1,23 @@
+package lu.oop.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    // Since Uncle Bob said that comments should be used carefully, I will disregard his advice
+    // So basically this is Spring Boot
+    // And in Spring boot everything is done through magical decorators
+    // How is this controller registered?
+    // Well rather simple - the @RestController
+    // It should be obvious to the untrained eye
+    // Ok next we do the "Hello worlds"
+    // We do it by giving request param
+    // And then we return the string
+    // TODO how to send JSON
+    @GetMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/lu/oop/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.oop.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
